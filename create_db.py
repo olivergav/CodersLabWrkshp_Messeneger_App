8724b3d76d673f3fcecb5ff3a937c43e04a7ba01
olivergav <oliwier.gawlik0@gmail.com>
2021-11-24 11:32:39 +0100

    Uzupełnienie pliku create_db.py. Stworzenie funkcji do wywołania.

--- create_db.py
+++ create_db.py
@@ -1,3 +1,46 @@
-from sql_utils import execute_sql
+from psycopg2 import connect, DatabaseError, OperationalError, errors
 
-# Importing execute_sql function from sql_utils
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-db", "--database", help="Please input database name")
+# parser.add_argument("-tb" "--table", help="Please input table name")
+# args = parser.parse_args()
+
+# Tworzenie obiektu kursora:
+conn = connect(user="postgres", password="coderslab", host="localhost", port=8888, database="library")
+conn.autocommit = True
+cur = conn.cursor()
+
+
+def create_database():
+    try:
+        name = str(input("Podaj nazwę bazy danych, którą chcesz stworzyć: "))
+        cur.execute(f"CREATE DATABASE {name}")
+    except errors.DuplicateDatabase as ex:
+        print("Błąd zapytania: ", ex)
+
+
+def create_table_user():
+    try:
+        cur.execute("""CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(255), hashed_password varchar(80));""")
+    except errors.DuplicateTable as ex:
+        print("Błąd zapytania: ", ex)
+
+
+def create_table_messages():
+    try:
+        cur.execute("""CREATE TABLE messages(id SERIAL PRIMARY KEY, from_id INT, to_id INT, creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, text VARCHAR(255))""")
+    except errors.DuplicateTable as ex:
+        print("Błąd zapytania: ", ex)
+
+
+def creating():
+    try:
+        create_database()
+        create_table_user()
+        create_table_messages()
+    except OperationalError as ex:
+        print("Błąd zapytania: ", ex)
+        raise
+
+# Chcąc wywołać wszystkie funkcje, wywołaj funkcję "creating()"
